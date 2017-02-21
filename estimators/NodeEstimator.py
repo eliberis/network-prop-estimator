ad0bec84ed5bfdbafa32000ef4c2a9e5000ea856
Edgar Liberis <edgaras.liberis@gmail.com>
2017-02-21 10:47:31 +0000

    Optimise node estimator

--- estimators/NodeEstimator.py
+++ estimators/NodeEstimator.py
@@ -4,13 +4,16 @@ from estimators.AbstractEstimator import AbstractEstimator
 class NodeEstimator(AbstractEstimator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Optimisation, avoids calling `degree` all the time.
+        # The graph doesn't change anyway, so it's OK to cache degrees.
+        self.degrees = {i: self.G.degree(i) for i in self.G.nodes_iter()}
 
     def _edge_weight_func(self, u, v):
-        return 1 / self.G.degree(u) + 1 / self.G.degree(v)
+        return 1 / self.degrees[u] + 1 / self.degrees[v]
 
     def _node_weight_func(self, u):
         vs = self.G.neighbors(u)
-        return 1 + sum(map(lambda v: 1 / self.G.degree(v), vs))
+        return 1 + sum(map(lambda v: 1 / self.degrees[v], vs))
 
     def _compute_metric(self, node, k, t):
         return t * self._node_weight_func(node) / (2 * k)
