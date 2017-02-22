799224bbf9cb4529ce5fd4d0b73d9916a7ef74fc
Edgar Liberis <edgaras.liberis@gmail.com>
2017-02-22 18:34:08 +0000

    Cache triangle computation results

--- estimators/FormulaTriangleEstimator.py
+++ estimators/FormulaTriangleEstimator.py
@@ -1,10 +1,12 @@
 from estimators.AbstractEstimator import AbstractEstimator
 from util import node_triangles
+from functools import lru_cache
 
 class FormulaTriangleEstimator(AbstractEstimator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @lru_cache(maxsize=None)
     def _accum_func(self, u):
         return node_triangles(self.G, u) / self.degrees[u]
 
--- estimators/WeightedTriangleEstimator.py
+++ estimators/WeightedTriangleEstimator.py
@@ -1,14 +1,17 @@
 from estimators.AbstractEstimator import AbstractEstimator
 from util import edge_triangles, node_triangles
+from functools import lru_cache
 
 class WeightedTriangleEstimator(AbstractEstimator):
     def __init__(self, *args, num_edges, **kwargs):
         self.num_edges = num_edges
         super().__init__(*args, **kwargs)
 
+    @lru_cache(maxsize=None)
     def _edge_weight_func(self, u, v):
         return 1 + edge_triangles(self.G, u, v)
 
+    @lru_cache(maxsize=None)
     def _node_weight_func(self, u):
         return self.degrees[u] + 2 * node_triangles(self.G, u)
 
