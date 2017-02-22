ff02327ba34441544d695586f7a56b8a8b833256
Edgar Liberis <edgaras.liberis@gmail.com>
2017-02-22 11:52:11 +0000

    Fix imports

--- estimators/__init__.py
+++ estimators/__init__.py
@@ -1,4 +1,4 @@
-from estimators.WeightedEdgeEstimator import *
+from estimators.WeightedNodeEstimator import *
 from estimators.WeightedEdgeEstimator import *
 from estimators.WeightedTriangleEstimator import *
 from estimators.FormulaNodeEstimator import *
